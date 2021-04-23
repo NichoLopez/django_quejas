6f04f26ec907ff95459b732cf2b676d7213c7c71
NichoLopez <viniciolm92@gmail.com>
2021-04-23 12:43:37 -0600

    Add files via upload

--- Aplicaciones/Gestion/views.py
+++ Aplicaciones/Gestion/views.py
@@ -1,3 +1,20 @@
+
+from django.http import HttpRequest
 from django.shortcuts import render
+from Aplicaciones.Gestion.forms import FormularioQueja
 
 # Create your views here.
+
+class FormularioQuejaView(HttpRequest):
+
+    def index(request):
+        queja = FormularioQueja()
+        return render(request, "QuejaIndex.html", {"form":queja})
+    
+    def procesar_formulario(request):
+        queja = FormularioQueja(request.POST)
+        if queja.is_valid():
+            queja.save()
+            queja = FormularioQueja()
+
+        return render(request, "QuejaIndex.html", {"form":queja, "mensaje":'OK'})
