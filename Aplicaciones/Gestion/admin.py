86bb9547ea49457d6daae45ef42e54b43cc9b0d5
NichoLopez <viniciolm92@gmail.com>
2021-04-23 12:42:40 -0600

    Add files via upload

--- Aplicaciones/Gestion/admin.py
+++ Aplicaciones/Gestion/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Gestion.models import Negocio, Departamento, Municipio, Departamento_municipio, Sucursal, Queja
+from Aplicaciones.Gestion.models import Negocio, Departamento, Municipio, Departamento_municipio, Sucursal, Queja
 # Register your models here.
 
 admin.site.register(Negocio)
